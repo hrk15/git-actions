5fd4566b642521ecd3dbb569ae891c1b8eba0c5f
hirak <hirak@Nitro5-Hirak.localdomain>
2020-06-09 14:27:22 +0530

    reduced line of code

--- utils/utils.js
+++ utils/utils.js
@@ -4,8 +4,7 @@ const chance = new Chance();
 const utilMethods = {
     getString: (value, opt = {}) => {
         const { length = 10 } = opt;
-        if (value.toLowerCase() === "random") return chance.string({ length })
-        else return value;
+        return value.toLowerCase() === "random" ? chance.string({ length }) : value;
     }
 };
 
