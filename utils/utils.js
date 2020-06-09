b1b51568ca3f454466ce80266cccac5440aa4b99
hirak <hirak@Nitro5-Hirak.localdomain>
2020-06-09 15:00:09 +0530

    fix for forced error

--- utils/utils.js
+++ utils/utils.js
@@ -4,7 +4,7 @@ const chance = new Chance();
 const utilMethods = {
     getString: (value, opt = {}) => {
         const { length = 10 } = opt;
-        return value.toLowerCase() === "random " ? chance.string({ length }) : value;
+        return value.toLowerCase() === "random" ? chance.string({ length }) : value;
     }
 };
 
